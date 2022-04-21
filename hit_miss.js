d7a2473c6192e2a29d941327b480df45474f75c3
Lottie Rugg <lottie.ruggeasey@gmail.com>
2022-04-21 11:15:47 +0100

    Changes implemented

--- hit_miss.js
+++ hit_miss.js
@@ -2,12 +2,24 @@
 let clickedItems = document.querySelectorAll('.game-item')
 
 clickedItems.forEach(function(clickedItem) {
-    clickedItem.addEventListener('click', function (e) {
-        if (e.currentTarget.classList.contains('game-board__carrot') || e.currentTarget.classList.contains('game-board__gopher')) {
-            return
-        } else if (e.currentTarget.classList.contains('miss')) {
-            e.currentTarget.classList.replace('game-board__sapling', 'game-board__carrot')
-            return
-        } e.currentTarget.classList.replace('game-board__sapling', 'game-board__gopher')
-        })
+    clickedItem.addEventListener('click', hitOrMiss)
+
 })
+
+/**
+ * This function changes game-board tiles on click based on hit or miss class
+ *
+ * @param e
+ */
+function hitOrMiss(e) {
+    let target = e.currentTarget.classList
+    if (target.contains('game-board__carrot') ||target.contains('game-board__gopher')) {
+        return
+    } else if (target.contains('miss')) {
+        target.replace('game-board__sapling', 'game-board__carrot')
+        return
+    }
+    target.replace('game-board__sapling', 'game-board__gopher')
+}
+
+
